32385bb62dd2be4fe86e33c7b8ce506eb125a68d
pantajosef <pantazidisjosef@hotmail.de>
2018-10-16 10:12:55 +0200

    add studenten_wg and wohnungen_ms to services

--- lib/jobs/wohnungs_job.rb
+++ lib/jobs/wohnungs_job.rb
@@ -9,30 +9,40 @@ require 'httparty'
 require 'nokogiri'
 
 class WohnungsJob
-  SERVICES = %i[wg_gesucht immoscout24 nadann immowelt]
-  SERVICE_VARIABLES = %i[@wg_gesucht @immoscout24 @nadann @immowelt]
+  SERVICES = %i[wg_gesucht immoscout24 nadann immowelt wohnungen_ms studenten_wg]
+  SERVICE_VARIABLES = %i[@wg_gesucht @immoscout24 @nadann @immowelt @wohnungen_ms @studenten_wg]
   INFO =
     {
       :@wg_gesucht => {
         url: 'https://www.wg-gesucht.de/wohnungen-in-Muenster.91.2.1.0.html?csrf_token=36037b68fb108af8b70cf623581392244af66633&offer_filter=1&noDeact=1&city_id=91&category=2&rent_type=2',
         container: %w[#main_content.row #main_column .panel.panel-default:not(.panel-hidden) .panel-body .row .col-sm-8 .list-details-panel-inner .detailansicht],
-        translation: 'WG Gesucht'
+        translation: 'WG Gesucht',
       },
       :@immoscout24 => {
         url: 'https://www.immobilienscout24.de/Suche/S-2/Wohnung-Miete/Umkreissuche/M_fcnster/-/-160749/2448287/-/1276010036/3/2,00-/40,00-/EURO--820,00',
         container: %w[#resultListItems .result-list__listing .result-list-entry__brand-title-container h5],
-        translation: 'ImmoScout24'
+        translation: 'ImmoScout24',
       },
       :@nadann => {
         url: 'https://www.nadann.de/rubriken/kleinanzeigen/biete-wohnen/',
         container: %w[#c10 section .row .col-xs-12 .card-columns .card .card-block .card-text span],
-        translation: 'NaDann'
+        translation: 'NaDann',
       },
       :@immowelt => {
         url: 'https://www.immowelt.de/liste/muenster/wohnungen/mieten?lat=51.95256&lon=7.63143&sr=3&roomi=2&rooma=3&prima=900&wflmi=40&sort=createdate%2Bdesc',
         container: %w[.immoliste .content_wrapper .iw_content .list_background_wrapper.padding_top_none_s .iw_list_content .js-object.listitem_wrap .listitem.clear .listcontent.clear h2.ellipsis],
-        translation: 'Immowelt'
-      }
+        translation: 'Immowelt',
+      },
+      :@wohnungen_ms => {
+        url: 'https://wohnungen.ms/provisionsfreie-immobilien-muenster/wohnungen-angebote/',
+        container: %w[#content article.post header h3],
+        translation: 'Wohnungen MS',
+      },
+      :@studenten_wg => {
+        url: 'https://www.studenten-wg.de/angebote_lesen.html?detailsuche=aus&preismode=&newsort=&stadt=M%FCnster&fuer=Wohnungen&mietart=1&mbsuche=Frauen+oder+M%E4nner&zimin=2&zimax=3',
+        container: %w[.property-container .property-text h3],
+        translation: 'Studenten WG',
+      },
     }
 
   attr_accessor(*(SERVICES + SERVICES.map {|s| :"#{s}_cache" }))
@@ -62,7 +72,7 @@ class WohnungsJob
       instance_variable_set var, instance_variable_get(cache_var)
       instance_variable_set cache_var, nil
 
-      puts(var.to_s.gsub('@', '').capitalize + ": " + instance_variable_get(var).strip)
+      puts(INFO[var][:translation] + ": " + instance_variable_get(var).strip)
     end
     puts '###################################################'
     puts '###################################################'
